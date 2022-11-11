016f96924b82b0685bdf5b5a1280f903353792d7
developer <you@example.com>
2022-11-11 18:41:58 +0530

    newone

---
+++ gitpar/jk.rb
@@ -0,0 +1,14 @@
+puts ("add operator"); 
+a = 25 ;
+b = 4
+puts (a+b) 
+puts ("subtract operator") 
+puts (a-b) 
+puts ("multiply operator") 
+puts (a*b) 
+puts ("division operator") 
+puts (a/b) 
+puts("exponetial operator") 
+puts(5 ** 3) 
+puts ("module operators " ) 
+puts (a%b) 
